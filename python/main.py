40f750c4f16858664c6014bf27db5dba81fc4e11
yuhiaoki <a-yuhi-build2022@mercari-partner.com>
2022-05-15 23:05:53 +0900

    0515

--- python/main.py
+++ python/main.py
@@ -4,6 +4,9 @@ import pathlib
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+# from typing import List,Dict
+import json
+from collections import OrderedDict
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
@@ -23,9 +26,24 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
+def add_item(name: str = Form(...),category: str = Form(...)):
+    with open('items.json') as f:
+        d= json.load(f, object_pairs_hook=OrderedDict)
+        print(d)
+        keys=['name','category']
+        values=[name,category]
+        item=dict(zip(keys,values))
+        d['items'].append(item)
+        print(d)
+    with open('items.json', 'w') as f:
+        json.dump(d, f, indent=2, ensure_ascii=False)
     return {"message": f"item received: {name}"}
+
+@app.get("/items")
+def get_items():
+    with open('items.json') as f:
+        d= json.load(f)
+    return d
 
 @app.get("/image/{items_image}")
 async def get_image(items_image):
