396cfa70dab071dadb9dbd2b961cf1f6c32c1bd6
yuhiaoki <a-yuhi-build2022@mercari-partner.com>
2022-05-22 13:07:42 +0900

    step5

--- python/models.py
+++ python/models.py
@@ -1,32 +1,3 @@
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base, SessionLocal
-from sqlalchemy_utils import UUIDType
-import uuid
-
-
-class Category(Base):
-    __tablename__ = "category"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    # children = relationship("items")
-
-
-class Items(Base):
-    __tablename__ = "items"
-
-    id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
-    name = Column(String)
-    category_id = Column(
-        Integer,
-        ForeignKey(
-            "category.id",
-        ),
-    )
-    image_filename = Column(String)
-
-
 # def main():
 #     url = "sqlite:///mercari.sqlite3"
 #     engine = sqlalchemy.create_engine(url, echo=True)
